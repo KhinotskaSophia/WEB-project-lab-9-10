3150c8eb0c2337c890958381485732cae3cbb55c
Khinotska Sofiia bc2025-7 <Sofiia.Khinotska@lnu.edu.ua>
2026-05-14 16:11:02 +0300

    Імплементація даних потягів та сервісу LocalStorage для бронювань

---
+++ src/data/trains.js
@@ -0,0 +1,42 @@
+export const trains = [
+  {
+    id: "1",
+    number: "045Д",
+    departureCity: "Харків-Пасажирський",
+    arrivalCity: "Ужгород",
+    departureTime: "2026-04-17T17:04",
+    arrivalTime: "2026-04-18T16:32"
+  },
+  {
+    id: "2",
+    number: "850Л",
+    departureCity: "Львів",
+    arrivalCity: "Рокитне-Волинське",
+    departureTime: "2026-04-17T07:23",
+    arrivalTime: "2026-04-17T14:17",
+  },
+  {
+    id: "3",
+    number: "012Л",
+    departureCity: "Львів",
+    arrivalCity: "Одеса-Головна",
+    departureTime: "2026-04-17T21:10",
+    arrivalTime: "2026-04-18T08:06",
+  },
+  {
+    id: "4",
+    number: "083Л",
+    departureCity: "Ужгород",
+    arrivalCity: "Дніпро-Головний",
+    departureTime: "2026-04-17T16:40",
+    arrivalTime: "2026-04-18T12:45",
+  },
+  {
+    id: "5",
+    number: "060Л",
+    departureCity: "Чоп",
+    arrivalCity: "Київ-Пасажирський",
+    departureTime: "2026-04-17T14:58",
+    arrivalTime: "2026-04-18T08:42",
+  }
+];
